0d134dcefc61e28cd6a2b612955d115462ebca28
Dmitry-Sokolovsky <dmitry635494@gmail.com>
2020-10-12 21:18:03 +0300

    float: Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = 0
+  let res = bracketsConfig.map(function (item) {
+    return item.join('');
+  });
+  
+  for (let i=0; a < res.length; i++) {
+    if (str.includes(res[a])) {
+      str = str.replace(res[a], '');
+      a = 0;
+    } else {
+      a++;
+    }
+  }
+
+  if(str){
+    return false;
+  }
+  else{
+    return true;
+  }  
 }
